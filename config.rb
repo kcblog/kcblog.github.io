3bca4681954bd499da09050973b0b4edbeafeb3d
Chin Lee <kwangchin@gmail.com>
2012-04-03 11:19:31 +0800

    update published path

--- config.rb
+++ config.rb
@@ -1,5 +1,10 @@
 project_type = :stand_alone
 
+http_path = "/"
+http_images_path = "/images"
+http_fonts_path = "/fonts"
+css_dir = "/stylesheets"
+
 sass_dir = "sass"
 images_dir = "source/images"
 fonts_dir = "source/fonts"
